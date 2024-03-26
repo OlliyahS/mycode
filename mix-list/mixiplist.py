c17dfb5c485af9a5217e8d678f87c4e9896cbf1c
OlliyahS <olliyahs@gmail.com>
2024-03-26 17:13:38 +0000

    mixed lists

---
+++ mix-list/mixiplist.py
@@ -0,0 +1,6 @@
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("The Ip address from this list is: " + iplist[3]) 
+print("The Ip address from this list is: " + iplist[4])
+print(' The Ip addresses from the list provided are: ' + iplist[3] + " and " +iplist[4])
+
